cd1176cf9bc60fd97c1aa091411ac62470e25c85
zyj <edenzyj88@gmail.com>
2023-11-11 18:11:00 +0800

    Change bulb color and luminance by js.

--- js/sa.js
+++ js/sa.js
@@ -1,23 +1,29 @@
  $(function(){
-    var iottalk_url = '';   // 'http://DomainName:port'; or 'https://DomainName';
-    var mqtt_url = ''; // 'ws://IP:port/mqtt'; or 'wss://DomainName:port/mqtts'; 
-    var mqtt_user =  '';
-    var mqtt_password = '';
+    var iottalk_url = 'https://class.iottalk.tw';   // 'http://DomainName:port'; or 'https://DomainName';
+    var mqtt_url = undefined; // 'ws://IP:port/mqtt'; or 'wss://DomainName:port/mqtts'; 
+    var mqtt_user =  'iottalk';
+    var mqtt_password = 'iottalk2023';
     var exec_interval = 500;
 
     var profile = {
-	    'dm_name': 'Dummy_Device',          
-		'idf_list':[Dummy_Sensor],
-		'odf_list':[Dummy_Control],
+	    'dm_name': 'Bulb',
+		'odf_list':[Luminance, Color_O],
 		'd_name': undefined,
     };
-		
-    function Dummy_Sensor(){
-        return Math.random();  // return undefined; // if no data to send
+
+    var lum;
+
+    function Luminance(data){
+        lum = data[0];
     }
 
-    function Dummy_Control(data){
-       $('.ODF_value')[0].innerText = data[0];
+    function Color_O(data){
+        var rr = Math.floor((data[0] * lum) / 100);
+        var gg = Math.floor((data[1] * lum) / 100);
+        var bb = Math.floor((data[2] * lum) / 100);
+        $('.bulb-top, .bulb-middle-1, .bulb-middle-2, .bulb-middle-3, .bulb-bottom, .night').css(
+            {'background': 'rgb('+ rr +', '+ gg +', '+ bb +')'}
+        );
     }
       
 /*******************************************************************/                
